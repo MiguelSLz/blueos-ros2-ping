43082a71b6cf1ecea5515108cada5d8ec36328a8
Miguel Sória da Luz <MiguelSoria348@gmail.com>
2025-07-04 16:07:18 -0300

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -13,7 +13,8 @@ tmux split-window -h
 tmux select-pane -t 0
 tmux split-window -h
 
-tmux send-keys -t 0 "ros2 launch mavros_control base.launch.py" Enter
+COMMAND_PANE_0="source /opt/ros/humble/setup.bash && source /home/ros2_ws/install/setup.bash && ros2 launch mavros_control base.launch.py"
+tmux send-keys -t 0 "${COMMAND_PANE_0}" Enter
 tmux send-keys -t 1 
 tmux send-keys -t 2 
 tmux send-keys -t 3 
@@ -22,11 +23,8 @@ function create_service {
     tmux new -d -s "$1" || true
     SESSION_NAME="$1:0"
     # Set all necessary environment variables for the new tmux session
-    for NAME in $(compgen -v | grep MAV_); do
-        VALUE=${!NAME}
-        tmux setenv -t $SESSION_NAME -g $NAME $VALUE
-    done
-    tmux send-keys -t $SESSION_NAME "$2" C-m
+    COMMAND_SERVICE="source /opt/ros/humble/setup.bash && source /home/ros2_ws/install/setup.bash && $2"
+    tmux send-keys -t $SESSION_NAME "${COMMAND_SERVICE}" C-m
 }
 
 create_service 'ttyd' 'ttyd -p 88 sh -c "/usr/bin/tmux attach -t ROS2 || /usr/bin/tmux new -s user_terminal"'
